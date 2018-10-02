a35f64281ef717d0fc875615052696d0a5b9d2c0
Stephen Weil <stephen.weil@gmail.com>
2018-10-01 20:36:43 -0500

    First rough draft of test (will check in on actual db2 tomorrow)

---
+++ spec/integration/limit_spec.rb
@@ -0,0 +1,41 @@
+describe 'a limit query', needs_live_db2_server: true do
+  before do
+    sequel do |db|
+      db.create_table!(:items) do
+        primary_key :id
+        String :name
+      end
+      class ::Item < Sequel::Model(db)
+      end
+    end
+  end
+
+  after do
+    sequel do |db|
+      db.drop_table?(:items)
+      Object.send(:remove_const, :Item)
+    end
+  end
+  
+  def sequel
+    connection_string = ENV.fetch('CONNECTION_STRING') {
+      raise 'You must define the CONNECTION_STRING environment variable and point it to a valid DB2 for IBM i database'
+    }
+    conn = Sequel.connect(connection_string)
+
+    yield conn
+  ensure
+    conn && conn.disconnect
+  end
+
+  context 'when using a cached dataset' do
+    it 'should return an array with the correct number of elements' do
+      sequel do |db|
+        # Sequel starts caching dataset query placeholder arguments on 3rd use
+        3.times { Items.create(name: 'N') }
+        some_items = Items.where(name: 'N')
+        3.times { expect(some_items.first(2).size).to eq(2) }
+      end
+    end
+  end
+end
