39cefd9e56ff6b6a0aa38b4193b9e77efe2a1980
Stephen Weil <stephen.weil@gmail.com>
2018-10-03 09:58:27 -0500

    Refactor test to use same assumptions as select_spec

--- spec/integration/limit_spec.rb
+++ spec/integration/limit_spec.rb
@@ -1,22 +1,4 @@
 describe 'a limit query', needs_live_db2_server: true do
-  before do
-    sequel do |db|
-      db.create_table!(:items) do
-        primary_key :id
-        String :name
-      end
-      class Item < Sequel::Model(db)
-      end
-    end
-  end
-
-  after do
-    sequel do |db|
-      db.drop_table?(:items)
-      Object.send(:remove_const, :Item)
-    end
-  end
-
   def sequel
     connection_string = ENV.fetch('CONNECTION_STRING') {
       raise 'You must define the CONNECTION_STRING environment variable and point it to a valid DB2 for IBM i database'
@@ -31,9 +13,9 @@ describe 'a limit query', needs_live_db2_server: true do
   context 'when using a cached dataset' do
     it 'should return an array with the correct number of elements' do
       # Sequel starts caching dataset query placeholder arguments on 3rd use
-      3.times { Item.create(name: 'N') }
-      some_items = Item.where(name: 'N')
-      3.times { expect(some_items.first(2).size).to eq(2) }
+      sequel do |db|
+        3.times { expect(db[:MITMAS].first(5).size).to eq(5) }
+      end
     end
   end
 end
